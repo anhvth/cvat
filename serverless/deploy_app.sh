8c915c28921d0c6f598d461fdb1c597315f94707
anhvo <anhvth.226@gmail.com>
2021-11-15 18:23:56 +0700

    update

--- serverless/deploy_app.sh
+++ serverless/deploy_app.sh
@@ -6,7 +6,7 @@ FUNCTIONS_DIR=${1:-$SCRIPT_DIR}
 
 nuctl create project cvat
 
-func_config=/home/anhvth/gitprojects/cvat/serverless/openvino/dextr/nuclio/function.yaml
+func_config=/home/anhvth/gitprojects/cvat/serverless/openvino/track-merger/nuclio/function.yaml
 func_root=$(dirname "$func_config")
 echo Deploying $(dirname "$func_root") function...
 nuctl deploy --project-name cvat --path "$func_root" \
---
+++ serverless/openvino/track-merger/execute_in_docker.sh
@@ -0,0 +1,1 @@
+docker exec -it nuclio-nuclio-vinai-trackmerger  $1
